237e4b5d8e96ed0c2f518cddbbbc1b6427a4b408
Coby <coby@lakefox.net>
2021-10-25 00:43:39 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,16 @@ let router = require('express').Router()
 let mailman = require('../config/nodemailer')
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', { title: 'voxlyte'})
 })
 
 router.get('/contact', (req, res) => {
-    res.render('contact')
+    res.render('contact', { title: 'connect with us'})
 })
 
 router.post('/contact', (req, res) => {
     let mailOptions = {
-        from: 'Postmaster <postmaster@voxlyte.com>',
+        from: `${req.boxy.name} <postmaster@voxlyte.com>`,
         to: 'customer.relations@voxlyte.com',
         subject: 'This is an automated message from the Voxlyte Website Contact Form',
         text: `Name: ${req.body.name} | Email: ${req.body.email}`
